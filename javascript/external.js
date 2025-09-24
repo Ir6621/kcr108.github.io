eb83e9eeea87fda0bb2871bfefdade1f19dfad8e
Ir6621 <ishraqrahman6621@gmail.com>
2025-09-24 15:55:14 -0500

    Saved username

--- javascript/external.js
+++ javascript/external.js
@@ -1,5 +1,7 @@
-arr = ["Mael","Eric","Randall","James"];
-for (var i=0; i < arr.length; i++)
-{
-  document.writeln(arr[i],"<br>");
+let username = window.prompt("Please enter your name:")
+
+if (username) {
+  document.write("Hello, welcome "+ username + "!");
+} else {
+  document.write("Hello, welcome guest!")
 }
